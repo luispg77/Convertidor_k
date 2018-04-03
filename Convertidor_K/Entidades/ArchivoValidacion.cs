c3062df1ef19ba64ce24ef5d6a21964d98644559
José Luis Pérez García <josepg@mx1.ibm.com>
2018-04-03 13:36:31 -0500

    Validación
    Creación de entidad de ArchivoValidación

---
+++ Convertidor_K/Entidades/ArchivoValidacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Convertidor_K.Entidades
+{
+    class ArchivoValidacion
+    {
+        public String origen { get; set; }
+        public String reemplazo { get; set; }
+    }
+}
