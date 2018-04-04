135aca39710f0bb8f8618f66343679710e63c95b
MarcoHernandezMedina <Marco.Hernandez.Medina@ibm.com>
2018-04-04 11:24:30 -0500

    PantallaAgregarValidacion
    Cambios Realizados

--- Convertidor_K/Entidades/ArchivoValidacion.cs
+++ Convertidor_K/Entidades/ArchivoValidacion.cs
@@ -7,6 +7,12 @@ namespace Convertidor_K.Entidades
 {
     class ArchivoValidacion
     {
+        public ArchivoValidacion() { }
+        public ArchivoValidacion(String origen, String reemplazo)
+        {
+            this.origen = origen;
+            this.reemplazo = reemplazo;
+        }
         public String origen { get; set; }
         public String reemplazo { get; set; }
     }
